0a39574839c83be164d7052fd2f98f6839abb143
baitieyi <baitieyi@163.com>
2019-04-30 15:42:30 +0800

    add antd

--- src/page/admin/app.js
+++ src/page/admin/app.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
+import Antd from 'ant-design-vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
-
+import './antd.less';
 
 import app from './app.vue';
 
 Vue.config.productionTip = false;
+
+Vue.use(Antd);
 
 // eslint-disable-next-line no-unused-vars
 const SAN = new Vue({
